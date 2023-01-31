3ff98e2412feac2bad15f61f4e52eb587dede5d1
TheNachoBIT <nacholopezcorvalan2@gmail.com>
2023-01-31 18:12:33 -0300

    Added App/Game Entry Point

---
+++ examples/app/appMain.rs
@@ -0,0 +1,9 @@
+pub struct AppMain {}
+
+impl AppMain
+{
+	pub fn Init()
+	{
+		println!("Hello World!");
+	}
+}
--- examples/hello.rs
+++ examples/hello.rs
@@ -1,9 +1,15 @@
 use geometria::graphics::core::GeoCore;
+pub mod app;
+use crate::app::appMain::AppMain;
+
+#[allow(non_snake_case)]
 
 fn main() {
     let mut core = GeoCore::init();
 
     let _geo_window = core.create_window("hello 1", 1280, 720);
 
-    core.run()
+    AppMain::Init();
+
+    core.run();
 }
